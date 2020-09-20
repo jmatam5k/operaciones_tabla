e5f79e6d5d29586c70c44b2336a474078252b04b
Jennifer Matamala <jmatam.go@gmail.com>
2020-09-20 19:49:42 -0300

    Se insertar 3 post, 2 para Pamela y 1 para Carlos

--- operaciones.sql
+++ operaciones.sql
@@ -4,4 +4,7 @@ CREATE DATABASE posts;
 --  Crear tabla post, con los atributos id, nombre de usuario, fecha de creación, contenido
 CREATE TABLE post(id SERIAL, usuario VARCHAR(50), fecha_creacion TIMESTAMP, contenido VARCHAR(140), descripcion VARCHAR(80));
 
-
+-- Insertar 3 post, 2 para el usuario "Pamela" y uno para "Carlos" 
+INSERT INTO post(usuario, fecha_creacion, contenido, descripcion) VALUES ('Pamela', current_timestamp, 'Contenido 1', 'Descripcion 1');
+INSERT INTO post(usuario, fecha_creacion, contenido, descripcion) VALUES ('Pamela', current_timestamp, 'Contenido 2', 'Descripcion 2');
+INSERT INTO post(usuario, fecha_creacion, contenido, descripcion) VALUES ('Carlos', current_timestamp, 'Contenido 1', 'Descripcion 1');
