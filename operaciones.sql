326bc0eb054599fa897ee7948ef3ab7f43d48c6f
Jennifer Matamala <jmatam.go@gmail.com>
2020-09-20 21:42:43 -0300

    Se realizan puntos 4, 5, 6, 7 y 8 del ejercicio, Donde se agrega titulo a la tabla post y sus publicaciones, se inserta 2 post a Pedro, se elimina el post de Carlos y se vuelve a ingresar un post a este último.

--- operaciones.sql
+++ operaciones.sql
@@ -1,10 +1,29 @@
--- Crear Base de datos posts 
+-- 1.- Crear Base de datos posts 
 CREATE DATABASE posts;
 
---  Crear tabla post, con los atributos id, nombre de usuario, fecha de creación, contenido
+-- 2.- Crear tabla post, con los atributos id, nombre de usuario, fecha de creación, contenido
 CREATE TABLE post(id SERIAL, usuario VARCHAR(50), fecha_creacion TIMESTAMP, contenido VARCHAR(140), descripcion VARCHAR(80));
 
--- Insertar 3 post, 2 para el usuario "Pamela" y uno para "Carlos" 
+-- 3.- Insertar 3 post, 2 para el usuario "Pamela" y uno para "Carlos" 
 INSERT INTO post(usuario, fecha_creacion, contenido, descripcion) VALUES ('Pamela', current_timestamp, 'Contenido 1', 'Descripcion 1');
 INSERT INTO post(usuario, fecha_creacion, contenido, descripcion) VALUES ('Pamela', current_timestamp, 'Contenido 2', 'Descripcion 2');
 INSERT INTO post(usuario, fecha_creacion, contenido, descripcion) VALUES ('Carlos', current_timestamp, 'Contenido 1', 'Descripcion 1');
+
+-- 4.- Modificar la tabla post, agregando la columna título
+ALTER TABLE post ADD titulo VARCHAR(30);
+
+-- 5. Agregar título a las publicaciones ya ingresadas
+UPDATE post SET titulo = 'Titulo 1' WHERE id = 1;
+UPDATE post SET titulo = 'Titulo 2' WHERE id = 2;
+UPDATE post SET titulo = 'Titulo 1' WHERE id = 3;
+
+-- 6. Insertar 2 post para el usuario "Pedro"
+INSERT INTO post(usuario, fecha_creacion, contenido, descripcion, titulo) VALUES ('Pedro', current_timestamp, 'Contenido 1', 'Descripcion 1', 'titulo 1');
+INSERT INTO post(usuario, fecha_creacion, contenido, descripcion, titulo) VALUES ('Pedro', current_timestamp, 'Contenido 2', 'Descripcion 2', 'titulo 2');
+
+-- 7. Eliminar el post de Carlos
+DELETE FROM post WHERE usuario = 'Carlos';
+
+-- 8. Ingresar un nuevo post para el usuario "Carlos"
+INSERT INTO post(usuario, fecha_creacion, contenido, descripcion, titulo) VALUES ('Carlos', current_timestamp, 'nuevo contenido', 'nueva descripcion', 'titulo');
+
